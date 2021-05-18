1ff65e1d20dab7d375a8354248591e14f96aad1d
Viktor Chan <victor.chan.2020g@gmail.com>
2021-05-18 11:27:49 +0300

    Updated according to the requirments from 1-7

--- Lect_1/map/map.sv
+++ Lect_1/map/map.sv
@@ -1,15 +1,29 @@
 class map;
   
-  int a_array[string];
+  local int a_array[string] = '{"apple" : 4,
+                   				"car" : 9,
+                   				"train" : 10};
   
+  //For all functions
+  //Successful operation - return 1
+  //Failed operation - return 0
   
-  
+  //Create new element in the array
   function bit put(input string key, input int value);
     
     a_array[key] = value;
     
+    if(a_array.exists(key)) begin
+      
+      return 1;
+      
+    end
+    
+    else return 0;
+    
   endfunction
   
+  //Print value from the array
   function int get(input string key);
     
     int flag = 0;
@@ -20,31 +34,42 @@ class map;
         
         $display("key %s have value %d", el, a_array[key]);
         flag = 1;
+        return a_array[key];
         
       end
       
     end
     
-    
      if(!flag) begin
         
        $display("Error");
-        
-        
+         
      end
     
-    
   endfunction
   
+  //Delete an element from the array 
   function bit delete(input string key);
   
-    a_array.delete(key);
+    if(a_array.exists(key)) begin
+      
+    	a_array.delete(key);
+      	return 1;
+    end
     
+    else return 0;
     
   endfunction
+      
+  //Prints values of the array    
+  function print();
   
-  
+    foreach(a_array[key]) begin
+
+      $display("key: %s      value: %d", key, a_array[key]);
+
+    end
+    
+  endfunction
+   
 endclass
-  
-  
-  
--- Lect_1/map/test.sv
+++ Lect_1/map/test.sv
@@ -2,37 +2,23 @@
 
 module sv_map();
   
-  
   initial begin
 
     map map = new;
 
-    map.a_array = '{"apple" : 4,
-                   "car" : 9,
-                   "train" : 10};
-
     //prints array before editing
-    foreach(map.a_array[key]) begin
+    map.print();
 
-      $display("key: %s      value: %d", key, map.a_array[key]);
-
-    end
-
+    //test
     map.put("truck", 11);
 
     map.get("apple");
 
     map.delete("train");
 
-
     //prints array after editing  
-    foreach(map.a_array[key]) begin
-
-      $display("key: %s      value: %d", key, map.a_array[key]);
-
-    end
-
-
+    map.print();
+    
   end
 
 endmodule
